440ec9fa4b602d3575740c3c3359b1e617181f83
koenwisse <koen@launchme.eu>
2021-12-14 20:06:34 +0100

    selectors and balance added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
+//import { useState } from "react";
+// We're importing the useSelector hook from react-redux, and the selectBalance selector that we defined previously.
+// That allows us to replace the useState call by assigning useSelector(selectBalance) to balance
+import { useDispatch, useSelector } from "react-redux";
 import { deposit } from "./store/balance/actions";
+import { selectBalance } from "./store/balance/selectors";
 
 export default function App() {
   const dispatch = useDispatch();
-  const [balance, setBalance] = useState(0);
+  // Now balance will be updated every time the computed value of our selector changes
+  // const [balance, setBalance] = useState(0);
+  const balance = useSelector(selectBalance);
 
   return (
     <div className="App">
---
+++ src/store/balance/selectors.js
@@ -0,0 +1,2 @@
+// define a selector function to extract the information we need from the store.
+export const selectBalance = (reduxState) => reduxState.balance.amount;
